ccfc2d97433ceb965a6e111beada11c8f7a44033
David Fernandes <devdkingg@gmail.com>
2024-07-17 19:10:21 -0300

    fix: name, splash and icon

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { WebView } from 'react-native-webview';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import Constants from 'expo-constants';
+
+const statusBarHeight = Constants.statusBarHeight;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor="#222425" />
+      <WebView source={{ uri: 'https://igrejafamiliaevida.com.br/' }} />
     </View>
   );
 }
@@ -13,8 +17,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });

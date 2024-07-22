f7daeffe10e35566f47cd5008a400ab379d4c203
David Fernandes <devdkingg@gmail.com>
2024-07-22 19:05:49 -0300

    fix: go back event

--- App.tsx
+++ App.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { WebView } from 'react-native-webview';
-import { StyleSheet, View, StatusBar } from 'react-native';
+import React, { useRef } from 'react';
+import { WebView, WebViewNavigation } from 'react-native-webview';
+import { StyleSheet, View, StatusBar, BackHandler } from 'react-native';
 import Constants from 'expo-constants';
+import { useBackHandler } from '@react-native-community/hooks';
 
 const statusBarHeight = Constants.statusBarHeight;
 
 export default function App() {
+  const webViewRef = useRef<WebView>(null);
+  const canGoBack = useRef(false);
+
+  useBackHandler(() => {
+    if (canGoBack.current && webViewRef.current) {
+      webViewRef.current.goBack();
+      return true; // Intercepts go back event
+    }
+    return false; // Does not inteccept go back event, default behaviour 
+  });
+
+  const handleNavigationStateChange = (navState: WebViewNavigation) => {
+    canGoBack.current = navState.canGoBack;
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#222425" />
-      <WebView source={{ uri: 'https://igrejafamiliaevida.com.br/' }} />
+      <WebView
+        ref={webViewRef}
+        source={{ uri: 'https://igrejafamiliaevida.com.br/' }}
+        onNavigationStateChange={handleNavigationStateChange}
+      />
     </View>
   );
 }
